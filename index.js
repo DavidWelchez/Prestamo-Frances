4322c42c50846931f566bd909b79c7b059b2692f
David <davidwelchez@gmail.com>
2020-10-03 01:37:49 -0600

    Implementacion de experess-handlebars como template engine y endpoint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 
 // Importar handlebars como template engine
-//const exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 // Importar body parser que nos permite acceder al cuerpo de la petición HTTP
 //const bodyParser = require("body-parser");
@@ -13,9 +13,9 @@ const express = require("express");
 const app = express();
 
 // Indicar a express utilizar handlebars como template engine
-//app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 
-//app.set("view engine", "hbs");
+app.set("view engine", "hbs");
 
 // Habilitar body parse para leer los datos del cuerpo de peticiones POST
 //app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,9 +23,9 @@ const app = express();
 // Crear una ruta para /
 // Información sobre los verbos HTTP
 // https://developer.mozilla.org/es/docs/Web/HTTP/Methods
-//app.get("/", (req, res, next) => {
-  //res.render("formulario_prestamo");
-//});
+app.get("/", (req, res, next) => {
+  res.render("formulario_prestamo");
+});
 
 //app.post("/prestamo", (req, res, next) => {
   // Asignación por destructuring
